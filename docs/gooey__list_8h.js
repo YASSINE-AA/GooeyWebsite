d9556bb4f81a98b5f9c54b14a7aeaf2121280921
YASSINE-AA <overflowtn@gmail.com>
2024-12-30 02:05:55 +0100

    docs update

--- docs/gooey__list_8h.js
+++ docs/gooey__list_8h.js
@@ -1,7 +1,9 @@
 var gooey__list_8h =
 [
-    [ "GooeyList_Add", "gooey__list_8h.html#afe0f2ae88c16a577de29923d95536a78", null ],
-    [ "GooeyList_AddItem", "gooey__list_8h.html#a93638c75b82cb99938d6a13fd32df0ce", null ],
+    [ "GooeyList_Add", "gooey__list_8h.html#a6a61ad2f3faabb1980ae7eb0e02d11dc", null ],
+    [ "GooeyList_AddItem", "gooey__list_8h.html#ae06d114ef89251aaf274fc6039ddfc15", null ],
     [ "GooeyList_Draw", "gooey__list_8h.html#a873e19eba44c5d17cc17d6ae84dac7c9", null ],
-    [ "GooeyList_HandleScroll", "gooey__list_8h.html#a6ea234721c4406f43f82fba551a6a3c5", null ]
+    [ "GooeyList_HandleClick", "gooey__list_8h.html#aea4640cc406dc7f1ad0c981c9606143c", null ],
+    [ "GooeyList_HandleScroll", "gooey__list_8h.html#a6ea234721c4406f43f82fba551a6a3c5", null ],
+    [ "GooeyList_HandleThumbScroll", "gooey__list_8h.html#abe05eca276f8b717141a0f5b7ef4a7a2", null ]
 ];
